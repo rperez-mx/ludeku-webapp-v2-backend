079d1e07d5cf59a9d5465dc8ebbb6f3d0e8b26d1
Ricardo Pérez <jobs.rperez@gmail.com>
2022-07-14 22:43:16 -0700

    Just created Strapi app for the backend

---
+++ config/middlewares.js
@@ -0,0 +1,33 @@
+module.exports = [
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+  'serve-react',
+  {
+    "timeout": 100,
+    "load": {
+      "before": [
+        "responseTime",
+        "logger",
+        "cors",
+        "responses",
+        "gzip"
+      ],
+      "order": [
+        "Define the middlewares' load order by putting their name in this array is the right order"
+      ],
+      "after": [
+        "parser",
+        "router",
+        "serve-react"
+      ]
+    }
+  }
+];
