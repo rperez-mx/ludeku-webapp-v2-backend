4c5ad63ba486738b19e0b42451f29ab583647a89
Ricardo Pérez <jobs.rperez@gmail.com>
2022-07-19 00:56:47 -0700

    Initial release

--- config/middlewares.js
+++ config/middlewares.js
@@ -9,25 +9,29 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-  'serve-react',
-  {
-    "timeout": 100,
-    "load": {
-      "before": [
-        "responseTime",
-        "logger",
-        "cors",
-        "responses",
-        "gzip"
-      ],
-      "order": [
-        "Define the middlewares' load order by putting their name in this array is the right order"
-      ],
-      "after": [
-        "parser",
-        "router",
-        "serve-react"
-      ]
-    }
-  }
+  // {
+  //   name: 'serve-react',
+  //   config: {
+  //     "timeout": 100,
+  //     "load": {
+  //       "before": [
+  //         "responseTime",
+  //         "logger",
+  //         "cors",
+  //         "responses",
+  //         "gzip"
+  //       ],
+  //       "order": [
+  //         "Define the middlewares' load order by putting their name in this array is the right order"
+  //       ],
+  //       "after": [
+  //         "parser",
+  //         "router",
+  //         "serve-react"
+  //       ]
+  //     }
+  //   }
+  //   ,
+  // }
+  
 ];
